a6371dc1f3d309c489f47b9dbbb34cf16bdb5e8c
coreykadd <coreymangan22@live.ie>
2018-02-06 15:42:16 +0000

    fixed tests for automation

--- tests/app-test.js
+++ tests/app-test.js
@@ -2,7 +2,7 @@ var expect = require('chai').expect;
 var request = require('request');
 
 describe('Connection', function(){
-    it('Status', function(done){
+    it('Status', function(){
         request('http://localhost:8080/', function(err, res, body){
             expect(res.statusCode).to.equal(200);
             done();
